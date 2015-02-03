d088a935aa5b7012a1871e70e1709f3f5593b847
Jake Schmitz <jake.d.schmitz@gmail.com>
2015-02-02 19:52:17 -0800

    now to start building form

--- client/templates/project/project-add.js
+++ client/templates/project/project-add.js
@@ -1,20 +1,26 @@
 Template.projectAdd.events({
   'submit .proposal': function(event) {
-    var name = event.target.name.value;
-    var description = event.target.name.value;
+    if (Meteor.user()) {
+      var name = event.target.name.value;
+      var description = event.target.name.value;
 
-    // Add project to database
-    Projects.insert({
-      name: name,
-      description: description
-    });
+      // Add project to database
+      Projects.insert({
+        name: name,
+        description: description,
+        createdBy: Meteor.user().username
+      });
 
-    // cleaning after database gets values
-    event.target.name.value = '';
-    event.target.url.value  = '';
+      // cleaning after database gets values
+      event.target.name.value = '';
+      event.target.url.value  = '';
 
-    // maybe have some sort of router redirect
-    Router.go('home');
+      // maybe have some sort of router redirect
+      Router.go('/');
+    } else {
+      alert('Must be logged in to add a project proposal');
+      Router.go('/');
+    }
     return false;
   }
 });
