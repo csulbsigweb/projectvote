98af1dbf1351c85fb916922f6d38166983427e6d
Jake Schmitz <jake.d.schmitz@gmail.com>
2015-02-03 23:12:50 -0800

    almost there, need to add voting properly

--- client/templates/project/project-add.js
+++ client/templates/project/project-add.js
@@ -14,20 +14,43 @@ Template.projectAdd.events({
         name: name,
         description: description,
         languages: langArray,
+        votes: 0,
+        voters: [],
         createdBy: Meteor.user().profile.name
       });
 
       // cleaning after database gets values
       event.target.name.value = '';
       event.target.description.value  = '';
-      event.target.tech.value = '';
 
       // maybe have some sort of router redirect
       Router.go('projects');
+      return false;
     } else {
       alert('Must be logged in to add a project proposal');
       Router.go('projects');
+      return false;
     }
-    return false;
+  },
+  'click .vote': function(event) {
+      if ($('.vote').hasClass('voteButton')) {
+        Projects.update(this._id, {
+          $inc: {
+            votes: 1
+          }
+        });
+
+        $('.vote').removeClass('voteButton');
+        $('.vote').addClass('unvoteButton');
+      } else {
+        Projects.update(this._id, {
+          $inc: {
+            votes: -1
+          }
+        });
+
+        $('.vote').removeClass('unvoteButton');
+        $('.vote').addClass('voteButton');
+      }
   }
 });
--- client/templates/project/projects.js
+++ client/templates/project/projects.js
@@ -1,5 +1,16 @@
 Template.projects.helpers({
   projects: function() {
-    return Projects.find({});
+    return Projects.find({}, {sort: {votes: -1}});
+  }
+});
+
+Template.projectVotes.events({
+  'click .vote': function(event) {
+      if (Meteor.user()) {
+        var name = Meteor.user().profile.name;
+        var idTag = '#' + this._id;
+
+        // TODO: add code similar to voting chapter from discover meteor
+      }
   }
 });
